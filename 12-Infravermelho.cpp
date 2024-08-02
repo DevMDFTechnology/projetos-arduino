ab9f1fdb3f8e5787aa69891320c28484c1b22910
DevMDFTechnology <desenvolvimento@mdftechnology.com.br>
2024-08-02 16:17:13 -0300

    Update 12-Infravermelho.cpp

--- 12-Infravermelho.cpp
+++ 12-Infravermelho.cpp
@@ -6,25 +6,32 @@
  * http://arcfn.com
  */
 
-
 #include <IRremote.h>
+
 #define emissorIR 3
 
 IRsend irsend;
 
 void setup() {
   Serial.begin(9600);
+  pinMode(emissorIR, OUTPUT);  // Configura o pino como saída
 }
 
 void loop() {
-  switch(Serial.read()){
-    case 'a':
-		  irsend.sendPanasonic(0x100BCBD, 48);
-      digitalWrite(emissorIR, HIGH);
-      delay(500);
-      digitalWrite(emissorIR, LOW);
-      Serial.println("Codigo Enviado");
-    delay(40);
+  if (Serial.available() > 0) {  // Verifica se há dados disponíveis na serial
+    char command = Serial.read();  // Lê o comando
+
+    switch (command) {
+      case 'a':
+        digitalWrite(emissorIR, HIGH);  // Liga o pino do emissor IR
+        delay(500);
+        digitalWrite(emissorIR, LOW);   // Desliga o pino do emissor IR
+        irsend.sendPanasonic(0x100BCBD, 48);  // Envia o código IR
+        Serial.println("Codigo Enviado");
+        delay(40);
+        break;
+    }
+
+    delay(5000);  // 5 segundos de atraso entre cada sinal
   }
-    delay(5000);  //5 second delay between each signal burst
 }
